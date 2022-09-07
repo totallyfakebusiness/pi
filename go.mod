35b1d20da61979183fc23ffcc1d0d3c70c6ce2e8
Patrick Easters <patrick.easters@sysdig.com>
2022-09-07 08:36:53 -0400

    Initial commit :birthday:

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/totallyfakebusiness/pi
+
+go 1.18
+
+require (
+	github.com/JJ/pigo v0.0.0-20170314090201-efb9838067fd
+	github.com/labstack/echo/v4 v4.9.0
+)
+
+require (
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		digits, _ := strconv.Atoi(c.QueryParam("digits"))
+		if digits < 1 {
+			digits = 1
+		}
+
+		return c.String(http.StatusOK, fmt.Sprintf("%f", pi(digits)))
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+// Taken from https://go.dev/doc/play/pi.go
+// pi launches n goroutines to compute an
+// approximation of pi.
+func pi(n int) float64 {
+	ch := make(chan float64)
+	for k := 0; k < n; k++ {
+		go term(ch, float64(k))
+	}
+	f := 0.0
+	for k := 0; k < n; k++ {
+		f += <-ch
+	}
+	return f
+}
+
+func term(ch chan float64, k float64) {
+	ch <- 4 * math.Pow(-1, k) / (2*k + 1)
+}
